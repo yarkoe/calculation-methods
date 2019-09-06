766ac8b09b4702760141d83c52e155630b4825f0
Koekin Yaroslav <ykoekin@gmail.com>
2019-09-06 22:59:37 +0300

    Added bisection method, renamed in separator.

---
+++ scripts/lab_work_1/roots_seeker/search_methods.py
@@ -0,0 +1,39 @@
+# _*_ coding: utf-8 _*_
+""" This file contains methods to search roots on the given segment.
+"""
+
+
+def find_root_bisection(math_function, eps, start, end):
+    """
+    This function finds root to within given epsilon.
+    @param math_function: source function.
+    @param eps: precision of found root.
+    @param start: start of segment when root would be found.
+    @param end: end of segment.
+    @return: tuple that contains the root, a count of steps that would be needed to find the root,
+    distance between the root and previous approximation, residual modulus.
+    """
+
+    current_segment_start = start
+    current_segment_end = end
+
+    counter = 0
+    while abs(current_segment_end - current_segment_start) < 2 * eps:
+        current_segment_mid = (current_segment_end - current_segment_start) / 2
+        if math_function(current_segment_start) * math_function(current_segment_mid) <= 0:
+            current_segment_end = current_segment_mid
+        else:
+            """ If math_function(current_segment_mid) * math_function(current_segment_end) <= 0
+            """
+            current_segment_start = current_segment_mid
+
+        counter += 1
+
+    approximate_root = (current_segment_end - current_segment_start) / 2
+    final_distance = abs(current_segment_start - approximate_root)
+    residual_modulus = abs(math_function(approximate_root))
+
+    return approximate_root, counter, final_distance, residual_modulus
+
+
+# TODO: realize methods: Newton method, modifiable Newton method and method of secants.
--- scripts/lab_work_1/roots_seeker/section_separator.py
+++ scripts/lab_work_1/roots_seeker/section_separator.py
@@ -1,33 +1,33 @@
 # _*_ coding: utf-8 _*_
 """ This file realize methods to separate
-a section by several. Each separated section contains
+a segment by several. Each separated segment contains
 an one root.
 """
 
 
-def separate_section(math_function, step_length, start, end):
+def separate_segment(math_function, step_length, start, end):
     """
 
-    Divide section by several. The rule of partition is f(a)*f(b) < 0
-    where a and b are start and end of the new sections. If this rule is true
-    the [a; b] will be a new separated section.
+    Divide segment by several. The rule of partition is f(a)*f(b) < 0
+    where a and b are start and end of the new segments. If this rule is true
+    the [a; b] will be a new separated segment.
     @param math_function: source function.
     @param step_length: length of one step.
-    @param start: start of section.
-    @param end: end of section
-    @return: the new sections that contain roots.
+    @param start: start of segment.
+    @param end: end of segment
+    @return: the new segments that contain roots.
     """
 
-    a = start
-    b = start + step_length
+    current_segment_start = start
+    current_segment_end = start + step_length
 
-    new_sections = []
+    new_segments = []
 
-    while b <= end:
-        if math_function(a) * math_function(b) <= 0:
-            new_sections.append((a, b))
+    while current_segment_end <= end:
+        if math_function(current_segment_start) * math_function(current_segment_end) <= 0:
+            new_segments.append((current_segment_start, current_segment_end))
 
-        a = b
-        b = a + step_length
+        current_segment_start = current_segment_end
+        current_segment_end = current_segment_start + step_length
 
-    return new_sections
+    return new_segments
