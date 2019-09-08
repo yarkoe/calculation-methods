fb7ef5f3f1242025d36991d441104bbb668932d8
Koekin Yaroslav <ykoekin@gmail.com>
2019-09-08 19:22:09 +0300

    Added newton method.

--- lab_work_1/lab_work_1/search_methods.py
+++ lab_work_1/lab_work_1/search_methods.py
@@ -3,12 +3,12 @@
 """
 
 
-def find_root_bisection(math_function, eps, start, end):
+def search_root_bisection(math_function, epsilon, start, end):
     """
-    This function finds root to within given epsilon.
+    This function searches root within given epsilon. Bisection method is used here.
     @param math_function: source function.
-    @param eps: precision of found root.
-    @param start: start of segment when root would be found.
+    @param epsilon: precision of found root.
+    @param start: start of segment where root would be found.
     @param end: end of segment.
     @return: tuple that contains the root, a count of steps that would be needed to find the root,
     distance between the root and previous approximation, residual modulus.
@@ -17,8 +17,8 @@ def find_root_bisection(math_function, eps, start, end):
     a = start
     b = end
 
-    counter = 0
-    while (2 * eps) < abs(b - a):
+    counter = 1
+    while (2 * epsilon) < abs(b - a):
         c = (a + b) / 2
         if math_function(a) * math_function(c) <= 0:
             b = c
@@ -33,7 +33,47 @@ def find_root_bisection(math_function, eps, start, end):
     final_distance = abs(a - approximate_root)
     residual_modulus = abs(math_function(approximate_root))
 
-    return approximate_root, (counter + 1), final_distance, residual_modulus
+    return approximate_root, counter, final_distance, residual_modulus
+
+
+def search_root_newton(math_function, derivative, epsilon, start, end):
+    """
+    This function searches root within given epsilon. Newton method is used here.
+    @param math_function: source function.
+    @param derivative: derivative of math_function.
+    @param epsilon: precision of found root.
+    @param start: start of segment where root would be found.
+    @param end: end of segment.
+    @return: tuple that contains the root, a count of steps that would be needed to find the root,
+    distance between the root and previous approximation, residual modulus.
+    """
+
+    first_point = end
+    while derivative(first_point) == 0.:
+        first_point = (start - first_point) / 2
+
+    root_multiplicity = 0
+    while True:
+        root_multiplicity += 1
+
+        approximate_root = first_point
+        cur_function_value = math_function(approximate_root)
+
+        counter = 1
+        while cur_function_value > epsilon:
+            cur_derivative_value = derivative(approximate_root)
+
+            if cur_derivative_value == 0.:
+                break
+
+            approximate_root = approximate_root - (cur_function_value / cur_derivative_value) * root_multiplicity
+
+            cur_function_value = math_function(approximate_root)
+            counter += 1
+        else:
+            residual_modulus = abs(cur_function_value)
+
+            return approximate_root, counter, residual_modulus
 
 
 # TODO: realize methods: Newton method, modifiable Newton method and method of secants.
--- lab_work_1/lab_work_1/tests/test_lab_work_1.py
+++ lab_work_1/lab_work_1/tests/test_lab_work_1.py
@@ -1,28 +1,29 @@
 import unittest
 
 from ..section_separator import separate_segment
-from ..search_methods import find_root_bisection
+from ..search_methods import search_root_bisection
 
 
-class RootsSeekerTest(unittest.TestCase):
+class TestQuadraticEquation(unittest.TestCase):
 
     def setUp(self):
-        self.__test_function = lambda x: x ** 2 - 1
+        self.__quadratic_function = lambda x: x ** 2 - 1
 
         self.__test_segment = (-2, 2)
         self.__test_step = (self.__test_segment[1] - self.__test_segment[0]) / 100
         self.__test_eps = 0.001
 
     def test_bisection_method(self):
-        segments = separate_segment(self.__test_function, self.__test_step, *self.__test_segment)
+        segments = separate_segment(self.__quadratic_function, self.__test_step, *self.__test_segment)
 
         self.assertEqual(len(segments), 2)
 
         roots = []
         for segment in segments:
-            roots.append(find_root_bisection(self.__test_function, self.__test_eps, *segment)[0])
+            roots.append(search_root_bisection(self.__quadratic_function, self.__test_eps, *segment)[0])
 
         self.assertAlmostEqual(roots[0], -1, delta=self.__test_eps)
+        self.assertAlmostEqual(roots[1], 1, delta=self.__test_eps)
 
 
 if __name__ == '__main__':
