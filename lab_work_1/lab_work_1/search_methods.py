31c5087128315b2ad1059374a9fd0e3300935e7a
Koekin Yaroslav <ykoekin@gmail.com>
2019-09-09 19:24:18 +0300

    Added trigonometric tests.

--- lab_work_1/lab_work_1/search_methods.py
+++ lab_work_1/lab_work_1/search_methods.py
@@ -17,8 +17,8 @@ def search_root_bisection(math_function, epsilon, start, end):
     a = start
     b = end
 
-    counter = 1
-    while (2 * epsilon) < abs(b - a):
+    step_counter = 1
+    while abs(b - a) > (2 * epsilon):
         c = (a + b) / 2
         if math_function(a) * math_function(c) <= 0:
             b = c
@@ -27,13 +27,13 @@ def search_root_bisection(math_function, epsilon, start, end):
             """
             a = c
 
-        counter += 1
+        step_counter += 1
 
     approximate_root = (a + b) / 2
     final_distance = abs(a - approximate_root)
     residual_modulus = abs(math_function(approximate_root))
 
-    return approximate_root, counter, final_distance, residual_modulus
+    return approximate_root, step_counter, final_distance, residual_modulus
 
 
 def search_root_newton(math_function, derivative, epsilon, start, end):
@@ -61,7 +61,7 @@ def search_root_newton(math_function, derivative, epsilon, start, end):
                                (math_function(pred_approximate_root) / derivative(pred_approximate_root)) * \
                                root_multiplicity
 
-        counter = 1
+        step_counter = 1
         while abs(new_approximate_root - pred_approximate_root) > epsilon:
             pred_approximate_root = new_approximate_root
 
@@ -74,12 +74,12 @@ def search_root_newton(math_function, derivative, epsilon, start, end):
                                    (math_function(pred_approximate_root) / pred_derivative_value) *\
                                    root_multiplicity
 
-            counter += 1
+            step_counter += 1
         else:
             residual_modulus = math_function(new_approximate_root)
             final_distance = abs(new_approximate_root - pred_approximate_root)
 
-            return new_approximate_root, counter, final_distance, residual_modulus
+            return new_approximate_root, step_counter, final_distance, residual_modulus
 
 
 def search_root_modifiable_newton(math_function, derivative, epsilon, start, end):
@@ -110,7 +110,7 @@ def search_root_modifiable_newton(math_function, derivative, epsilon, start, end
                                (math_function(pred_approximate_root) / first_derivative_value) * \
                                root_multiplicity
 
-        counter = 1
+        step_counter = 1
         while abs(new_approximate_root - pred_approximate_root) > epsilon:
             pred_approximate_root = new_approximate_root
 
@@ -118,11 +118,45 @@ def search_root_modifiable_newton(math_function, derivative, epsilon, start, end
                                    (math_function(pred_approximate_root) / first_derivative_value) * \
                                    root_multiplicity
 
-            counter += 1
+            step_counter += 1
         else:
-            residual_modulus = math_function(new_approximate_root)
             final_distance = abs(new_approximate_root - pred_approximate_root)
+            residual_modulus = math_function(new_approximate_root)
 
-            return new_approximate_root, counter, final_distance, residual_modulus
+            return new_approximate_root, step_counter, final_distance, residual_modulus
 
-# TODO: realize methods: modifiable Newton method and method of secants.
+
+def search_root_secants_method(math_function, epsilon, start, end):
+    """
+    This function searches root within given epsilon. Modifiable newton method is used here.
+    @param math_function: source function.
+    @param epsilon: precision of found root.
+    @param start: start of segment where root would be found.
+    @param end: end of segment.
+    @return: tuple that contains the root, a count of steps that would be needed to find the root,
+    distance between the root and previous approximation, residual modulus.
+    """
+
+    pred_approximate_root = start
+    new_approximate_root = end
+
+    step_counter = 1
+    while abs(new_approximate_root - pred_approximate_root) > epsilon:
+        new_function_value = math_function(new_approximate_root)
+
+        temp_new_approximate_root = new_approximate_root
+        new_approximate_root = new_approximate_root - new_function_value * \
+                               (new_approximate_root - pred_approximate_root) / \
+                               (new_function_value - math_function(pred_approximate_root))
+
+        pred_approximate_root = temp_new_approximate_root
+
+        step_counter += 1
+
+    final_distance = abs(new_approximate_root - pred_approximate_root)
+    residual_modulus = math_function(new_approximate_root)
+
+    return new_approximate_root, step_counter, final_distance, residual_modulus
+
+
+# TODO: realize methods: method of secants.
--- lab_work_1/lab_work_1/tests/test_lab_work_1.py
+++ lab_work_1/lab_work_1/tests/test_lab_work_1.py
@@ -1,7 +1,9 @@
 import unittest
+import math
 
 from ..section_separator import separate_segment
-from ..search_methods import search_root_bisection, search_root_newton, search_root_modifiable_newton
+from ..search_methods import search_root_bisection, search_root_newton, \
+                             search_root_modifiable_newton, search_root_secants_method
 
 
 class TestQuadraticEquation(unittest.TestCase):
@@ -43,6 +45,65 @@ class TestQuadraticEquation(unittest.TestCase):
 
         self.assertAlmostEqual(roots[0], -1, delta=self.test_eps)
         self.assertAlmostEqual(roots[1], 1, delta=self.test_eps)
+
+    def test_secants_method(self):
+
+        roots = []
+        for segment in self.segments:
+            roots.append(search_root_secants_method(self.quadratic_function, self.test_eps, *segment)[0])
+
+        self.assertAlmostEqual(roots[0], -1, delta=self.test_eps)
+        self.assertAlmostEqual(roots[1], 1, delta=self.test_eps)
+
+
+class TestTrigonometricEquation(unittest.TestCase):
+
+    def setUp(self):
+        self.trigonometric_function = lambda x: x * math.sin(x) - 1
+        self.derivative = lambda x: math.sin(x) + x * math.cos(x)
+
+        self.test_segment = (-10, 2)
+        self.test_step = (self.test_segment[1] - self.test_segment[0]) / 100
+        self.test_eps = 10 ** (-5)
+
+        self.segments = separate_segment(self.trigonometric_function, self.test_step, *self.test_segment)
+
+        self.assertEqual(len(self.segments), 5)
+
+    def test_bisection_method(self):
+        roots = []
+        for segment in self.segments:
+            roots.append(search_root_bisection(self.trigonometric_function, self.test_eps, *segment)[0])
+
+        self.assertAlmostEqual(roots[0], -9.31724, delta=self.test_eps)
+        self.assertAlmostEqual(roots[1], -6.43911, delta=self.test_eps)
+
+    def test_newton_method(self):
+        roots = []
+        for segment in self.segments:
+            roots.append(search_root_newton(self.trigonometric_function, self.derivative, self.test_eps, *segment)[0])
+
+        self.assertAlmostEqual(roots[0], -9.31724, delta=self.test_eps)
+        self.assertAlmostEqual(roots[1], -6.43911, delta=self.test_eps)
+
+    def test_modifiable_newton_method(self):
+
+        roots = []
+        for segment in self.segments:
+            roots.append(search_root_modifiable_newton(self.trigonometric_function,
+                                                       self.derivative, self.test_eps, *segment)[0])
+
+        self.assertAlmostEqual(roots[0], -9.31724, delta=self.test_eps)
+        self.assertAlmostEqual(roots[1], -6.43911, delta=self.test_eps)
+
+    def test_secants_method(self):
+
+        roots = []
+        for segment in self.segments:
+            roots.append(search_root_secants_method(self.trigonometric_function, self.test_eps, *segment)[0])
+
+        self.assertAlmostEqual(roots[0], -9.31724, delta=self.test_eps)
+        self.assertAlmostEqual(roots[1], -6.43911, delta=self.test_eps)
 
 
 if __name__ == '__main__':
