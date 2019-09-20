1624f56f106f47bbe01ce9c5cbc798f80a9c6a14
Koekin Yaroslav <ykoekin@gmail.com>
2019-09-20 22:40:35 +0300

    Added Newton form and tests.

--- lab_work_2/lab_work_2/polynomial_functions.py
+++ lab_work_2/lab_work_2/polynomial_functions.py
@@ -6,34 +6,55 @@ def create_lagrangian_form(table, polynomial_degree):
     @return: approximating polynomial function.
     """
 
-    def w(x):
-        result = 1
-        for i in range(polynomial_degree + 1):
-            result *= x - table[i][0]
-
-        return result
-
-    def derivative_w(k):
+    def z(x, k):
         result = 1
         for i in range(polynomial_degree + 1):
             if i == k:
                 continue
 
-            result *= table[k][0] - table[i][0]
+            result *= x - table[i][0]
 
         return result
 
     def l(x, k):
-        try:
-            return w(x) / ((x - table[k][0]) * derivative_w(k))
-        except ZeroDivisionError:
-            # if (x - table[k][0]) ~ 0
-            return 1
+        return z(x, k) / z(table[k][0], k)
 
     def p(x):
         sum = 0
         for k in range(polynomial_degree + 1):
             sum += l(x, k) * table[k][1]
+
+        return sum
+
+    return p
+
+
+def create_newton_form(table, polynomial_degree):
+    """
+    Creates polynomial in the Newton form.
+    @param table: list of (node, function_value) tuples.
+    @param polynomial_degree: the degree of the polynomial that should be created.
+    @return: approximating polynomial function.
+    """
+
+    divided_differences_table = [tuple(table[i][1] for i in range(polynomial_degree + 1)), ]
+
+    for i in range(1, polynomial_degree + 1):
+        divided_differences_table.append(
+            tuple((divided_differences_table[i-1][j + 1] - divided_differences_table[i-1][j]) /
+                  (table[j + i][0] - table[j][0]) for j in range(polynomial_degree + 1 - i)))
+
+    def w(x, k):
+        result = 1
+        for i in range(k):
+            result *= x - table[i][0]
+
+        return result
+
+    def p(x):
+        sum = 0
+        for i in range(polynomial_degree + 1):
+            sum += divided_differences_table[i][0] * w(x, i)
 
         return sum
 
--- lab_work_2/lab_work_2/tests/test_lab_work_2.py
+++ lab_work_2/lab_work_2/tests/test_lab_work_2.py
@@ -2,7 +2,7 @@ import unittest
 import math
 
 from ..table_operations import create_table, sort_table
-from ..polynomial_functions import create_lagrangian_form
+from ..polynomial_functions import create_lagrangian_form, create_newton_form
 
 
 class TestPolynomialFunctions(unittest.TestCase):
@@ -16,11 +16,17 @@ class TestPolynomialFunctions(unittest.TestCase):
             "m": 15
         }
 
-    def test_lagrangian_form(self):
-        table = create_table(*(self.test_set_1[el] for el in ("function", "m", "start", "end")))
-        table = sort_table(table, self.test_set_1["x"])
+        self.test_table_1 = create_table(*(self.test_set_1[el] for el in ("function", "m", "start", "end")))
+        self.test_table_1 = sort_table(self.test_table_1, self.test_set_1["x"])
 
-        lagrangian_form = create_lagrangian_form(table, self.test_set_1["n"])
+    def test_lagrangian_form(self):
+        lagrangian_form = create_lagrangian_form(self.test_table_1, self.test_set_1["n"])
 
         for i in range(self.test_set_1["n"] + 1):
-            self.assertAlmostEqual(table[i][1], lagrangian_form(table[i][0]))
+            self.assertAlmostEqual(self.test_table_1[i][1], lagrangian_form(self.test_table_1[i][0]))
+
+    def test_newton_form(self):
+        newton_form = create_newton_form(self.test_table_1, self.test_set_1["n"])
+
+        for i in range(self.test_set_1["n"] + 1):
+            self.assertAlmostEqual(self.test_table_1[i][1], newton_form(self.test_table_1[i][0]))
