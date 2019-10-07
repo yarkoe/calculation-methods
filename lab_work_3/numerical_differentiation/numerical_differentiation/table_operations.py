e7841c535443558f408d35688174b94bed8c778f
Koekin Yaroslav <ykoekin@gmail.com>
2019-10-07 23:40:41 +0300

    Realized differentiation table creation.

--- lab_work_3/numerical_differentiation/numerical_differentiation/table_operations.py
+++ lab_work_3/numerical_differentiation/numerical_differentiation/table_operations.py
@@ -1,3 +1,6 @@
+from .differentiation_methods import *
+
+
 def create_table(function, nodes_number, start, step):
     """
     Creates a new table. View of the table's row is a pair (node, function(node))
@@ -28,11 +31,37 @@ def create_differentiation_table(function, derivative, second_derivative, nodes_
     @return: extended table.
     """
 
-    table = create_table(function, nodes_number - 1, start, step)  # creates table of (node, function(node)) tuples.
+    table = create_table(function, nodes_number, start, step)  # creates table of (node, function(node)) tuples.
 
-    extended_table = []
+    differentiation_table = []
 
     if len(table) < 3:
-        return extended_table
+        return differentiation_table
 
-    
+    # creating the first row of the extended table.
+    first_approximate_derivative = differentiate_first(table[0][1], table[1][1], table[2][1], step)
+    first_derivative_diff = abs(derivative(table[0][0]) - first_approximate_derivative)
+
+    # creating the last row of the extended table.
+    last_approximate_derivative = differentiate_last(table[nodes_number-1][1], table[nodes_number-2][1],
+                                                     table[nodes_number-3][1], step)
+    last_derivative_diff = abs(derivative(table[nodes_number-1][0]) - last_approximate_derivative)
+
+    # creating the center rows of the extended table.
+    center_table = []
+    for i in range(1, nodes_number - 1):
+        approximate_derivative = differentiate_center(table[i+1][1], table[i-1], step)
+        derivative_diff = abs(derivative(table[i][0]) - approximate_derivative)
+
+        approximate_second_derivative = differentiate_center_twice(table[i][1], table[i+1][1], table[i-1][1], step)
+        second_derivative_diff = abs(second_derivative(table[i][0]) - approximate_second_derivative)
+
+        center_table.append((*table[i], approximate_derivative, derivative_diff, approximate_second_derivative,
+                             second_derivative_diff))
+
+    # results assembling.
+    differentiation_table.append((*table[0], first_approximate_derivative, first_derivative_diff))
+    differentiation_table.extend(center_table)
+    differentiation_table.append((*table[nodes_number-1], last_approximate_derivative, last_derivative_diff))
+
+    return differentiation_table
