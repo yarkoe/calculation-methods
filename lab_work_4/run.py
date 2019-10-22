cf88fd61bd5140b6d9f235dd53131facf0565ee9
Koekin Yaroslav <ykoekin@gmail.com>
2019-10-22 09:58:59 +0300

    Show functions were added.

---
+++ lab_work_4/run.py
@@ -0,0 +1,67 @@
+import math
+
+from lab_work_4.lab_work_4.integral_by_QF import QFIntegralContainer
+
+
+def input_int(message):
+    while True:
+        try:
+            int_value = int(input(message))
+            if int_value <= 0:
+                print("Значение должно быть больше 0")
+                continue
+            break
+        except ValueError:
+            print("Неверный формат")
+
+    return int_value
+
+
+def input_float(message):
+    while True:
+        try:
+            float_value = float(input(message))
+            break
+        except ValueError:
+            print("Неверный формат")
+
+    return float_value
+
+
+def print_qf_method(method_name, approximate_value, true_value):
+    print(method_name)
+    print("Приближённое значение: {}".format(approximate_value))
+    print("Абсолютная фактическая погрешность: {}\n".format(abs(true_value - approximate_value)))
+
+
+def main():
+    print("Лабораторная работа № 4")
+    print("Приближённое вычисление интеграла по составным квадратурным формулам")
+
+    def function(x):
+        return 2 * math.sin(x) - x / 2
+
+    def antiderivative(x):
+        return -2 * math.cos(x) - x ** 2 / 4
+
+    while True:
+        start = input_float("Введите начало отрезка: ")
+        end = input_float("Введите конец отрезка: ")
+        print("Вид фукнции: f(x) = 2*sin(x) - x/2")
+        print("A= {}, B= {}\n".format(start, end))
+
+        true_value = antiderivative(end) - antiderivative(start)
+        print("Значение интеграла от функции f по отрезку [A; B]= {}\n".format(true_value))
+
+        m = input_int("Введите m: ")
+        qf_integral_container = QFIntegralContainer(function, (start, end), m)
+
+        print_qf_method("Метод левых прямоугольников", qf_integral_container.calculate_by_left_rec(), true_value)
+        print_qf_method("Метод правых прямоугольников", qf_integral_container.calculate_by_right_rec(), true_value)
+        print_qf_method("Метод средних прямоугольников", qf_integral_container.calculate_by_average_rec(), true_value)
+        print_qf_method("Метод трапеций", qf_integral_container.calculate_by_trap(), true_value)
+        print_qf_method("Метод Симпсона", qf_integral_container.calculate_by_simpson(), true_value)
+
+
+if __name__ == "__main__":
+    main()
